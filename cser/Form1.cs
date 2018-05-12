56fbcc2fa4daa9947d0907452c234f13eaf35fd0
Gang Zhang <gzhangx@hotmail.com>
2018-05-12 16:05:58 -0400

    initia

---
+++ cser/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        scom comm = new scom();
+        private void Start_Click(object sender, EventArgs e)
+        {
+            comm.Start();           
+        }
+    }
+}
---
+++ cser/scom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cser
+{
+    class scom
+    {
+        protected SerialPort comm = new SerialPort();
+        public scom()
+        {
+            comm.ReadTimeout = 500;
+            comm.WriteTimeout = 500;
+            comm.BaudRate = 128000;
+            comm.PortName = "com3";
+        }
+        public void Start()
+        {                       
+            comm.Open();
+        }
+    }
+}
